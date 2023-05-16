e861563a81d57807a57c9e3dd0b191b72a05d35d
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-16 19:52:44 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+
+# methods.rb
+
+# Method 1: greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  # Method 2: greet
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  # Method 3: greet_with_default
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  # Method 4: add
+  def add(num1, num2)
+    num1 + num2
+  end
+  
+  # Method 5: halve
+  def halve(number)
+    return nil unless number.is_a?(Numeric)
+  
+    number / 2
+  end
+  
